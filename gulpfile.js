a3fff3c367bc9f7a92da2d593412167d09165454
Jen J Wong <jenjwong@gmail.com>
2016-10-24 23:20:23 -0700

    Refactors files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('styles', function(){
-  gulp.src('newsFeed.css')
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('build'))
-});
